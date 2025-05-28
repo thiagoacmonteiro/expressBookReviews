8c3620c2efce34ece48b62426241426060296598
Thiago Monteiro <thiagoaucmonteiro@gmail.com>
2025-05-28 08:47:15 -0300

    :sparkles:feat: Implement tasks 11, 12 and 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,40 +43,100 @@ public_users.get("/", async function (_req, res) {
   }
 });
 
+async function getBookDetailsByISBN(isbn) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() > 0.3;
+
+      const booksArray = Object.values(books);
+      const bookDetails = booksArray.find((item) => item.isbn === isbn);
+
+      if (success) {
+        resolve(bookDetails);
+      } else {
+        reject(new Error("Error getting book details by isbn"));
+      }
+    }, 3000);
+  });
+}
+
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
   const { isbn } = req.params;
 
-  const booksArray = Object.values(books);
-  const bookDetails = booksArray.find((item) => item.isbn === isbn);
+  try {
+    const bookDetails = await getBookDetailsByISBN(isbn);
 
-  if (bookDetails) res.status(200).json(bookDetails);
+    if (bookDetails) return res.status(200).json(bookDetails);
 
-  return res.status(200).json({ message: "Book not found by given isbn" });
+    return res.status(200).json({ message: "Book not found by given isbn" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
+
+async function getBookDetailsByAuthor(author) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() > 0.3;
+
+      const booksArray = Object.values(books);
+      const bookDetails = booksArray.find((item) => item.author === author);
+
+      if (success) {
+        resolve(bookDetails);
+      } else {
+        reject(new Error("Error getting book details by author"));
+      }
+    }, 3000);
+  });
+}
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
   const { author } = req.params;
 
-  const booksArray = Object.values(books);
-  const bookDetails = booksArray.find((item) => item.author === author);
+  try {
+    const bookDetails = await getBookDetailsByAuthor(author);
 
-  if (bookDetails) res.status(200).json(bookDetails);
+    if (bookDetails) return res.status(200).json(bookDetails);
 
-  return res.status(200).json({ message: "Book not found by given author" });
+    return res.status(200).json({ message: "Book not found by given author" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
+async function getBookDetailsByTitle(title) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() > 0.3;
+
+      const booksArray = Object.values(books);
+      const bookDetails = booksArray.find((item) => item.title === title);
+
+      if (success) {
+        resolve(bookDetails);
+      } else {
+        reject(new Error("Error getting book details by title"));
+      }
+    }, 3000);
+  });
+}
+
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   const { title } = req.params;
 
-  const booksArray = Object.values(books);
-  const bookDetails = booksArray.find((item) => item.title === title);
+  try {
+    const bookDetails = await getBookDetailsByTitle(title);
 
-  if (bookDetails) res.status(200).json(bookDetails);
+    if (bookDetails) return res.status(200).json(bookDetails);
 
-  return res.status(200).json({ message: "Book not found by given title" });
+    return res.status(200).json({ message: "Book not found by given title" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 //  Get book review
@@ -86,7 +146,7 @@ public_users.get("/review/:isbn", function (req, res) {
   const booksArray = Object.values(books);
   const bookDetails = booksArray.find((item) => item.isbn === isbn);
 
-  if (bookDetails) res.status(200).json(bookDetails.reviews);
+  if (bookDetails) return res.status(200).json(bookDetails.reviews);
 
   return res.status(200).json({ message: "Book not found by given isbn" });
 });
