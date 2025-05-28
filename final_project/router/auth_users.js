dec4fa51155520b1273ae78685c69b30e49decff
Thiago Monteiro <thiagoaucmonteiro@gmail.com>
2025-05-28 07:29:14 -0300

    :sparkles:feat: Implement login with session

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,8 +17,32 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      {
+        data: { username, password },
+      },
+      "secret",
+      { expiresIn: 3600 }
+    );
+
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+
+    return res.status(200).send({ message: "User successfully logged in" });
+  } else {
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
